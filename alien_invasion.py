3e7c8ac6fded5121e31ed3e0aef435ee8faf7246
zhangxv <zhangxv95@163.com>
2019-03-13 11:26:23 +0800

    提交

--- alien_invasion.py
+++ alien_invasion.py
@@ -48,7 +48,8 @@ def run_game():
             gf.update_aliens(ai_settings, screen, stats,
                              sb, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, stats, sb,ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb,
+                         ship, aliens, bullets, play_button)
 
 
 run_game()
